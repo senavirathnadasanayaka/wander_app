bd67d6fd6675ae0abc31b5d11801f06a49129b13
senavirathnadasanayaka <120745110+senavirathnadasanayaka@users.noreply.github.com>
2023-04-30 19:49:50 -0700

    now begin

--- screens/Hotels/hotels.js
+++ screens/Hotels/hotels.js
@@ -4,7 +4,7 @@ const hotels = [
     name: "Palmyra Nature Resort Sigiriya",
     location: "No 207/A,Bandaranayaka Rd,Sigiriya",
     price: 120,
-    image: require("../../assets/Hotel1.jpg"),
+    image: require("../../assets/hotel1.jpg"),
     details: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Consequat nisl vel pretium lectus quam id leo. Velit euismod in pellentesque massa placerat duis ultricies lacus sed. Justo laoreet sit amet cursus sit`,
   },
   {
--- screens/Shorttrip/Short.js
+++ screens/Shorttrip/Short.js
@@ -1,56 +1,54 @@
-import {
-  add,
-  Canvas,
-  Circle,
-  LinearGradient,
-  vec,
-  sub,
-  Fill,
-  useLoop,
-  mix,
-  BackdropFilter,
-  Blur,
-  useComputedValue,
-} from "@shopify/react-native-skia";
-import React, { useMemo } from "react";
-import { useWindowDimensions } from "react-native";
+import React, { useState, useEffect } from 'react';
+import { View, Text } from 'react-native';
+import MapView, { Marker } from 'react-native-maps';
+import { API_KEY } from 'your_api_key';
 
-export const Short = () => {
-  const { width, height } = useWindowDimensions();
-  const c = vec(width / 2, height / 2);
-  const r = c.x - 32;
-  const rect = useMemo(
-    () => ({ x: 0, y: c.y, width, height: c.y }),
-    [c.y, width]
-  );
 
-  const progress = useLoop({ duration: 2000 });
-  const start = useComputedValue(
-    () => sub(c, vec(0, mix(progress.current, r, r / 2))),
-    [progress]
-  );
-  const end = useComputedValue(
-    () => add(c, vec(0, mix(progress.current, r, r / 2))),
-    []
-  );
-  const radius = useComputedValue(
-    () => mix(progress.current, r, r / 2),
-    [progress]
-  );
+const Short = () => {
+  const [review, setReview] = useState(null);
+  const { latitude, longitude } = location;
 
+  useEffect(() => {
+    fetch(`https://api.yelp.com/v3/businesses/search?latitude=${latitude}&longitude=${longitude}`, {
+      headers: {
+        Authorization: `Bearer ${API_KEY}`,
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        setReview(data.businesses[0]);
+      })
+      .catch((error) => console.error(error));
+  }, [latitude, longitude]);
   return (
-    <Canvas style={{ flex: 1 }}>
-      <Fill color="black" />
-      <Circle c={c} r={radius}>
-        <LinearGradient
-          start={start}
-          end={end}
-          colors={["#FFF723", "#E70696"]}
-        />
-      </Circle>
-      <BackdropFilter filter={<Blur blur={10} />} clip={rect}>
-        <Fill color="rgba(0, 0, 0, 0.3)" />
-      </BackdropFilter>
-    </Canvas>
-  );
-};
+    <View style={{ flex: 1 }}>
+    <MapView
+      style={{ flex: 1 }}
+      initialRegion={{
+        latitude: latitude,
+        longitude: longitude,
+        latitudeDelta: 0.0922,
+        longitudeDelta: 0.0421,
+      }}
+    >
+      <Marker
+        coordinate={{ latitude: latitude, longitude: longitude }}
+        title={review?.name}
+        description={review?.categories.map((category) => category.title).join(', ')}
+      />
+    </MapView>
+    {review && (
+      <View style={{ backgroundColor: 'white', padding: 10 }}>
+        <Text style={{ fontWeight: 'bold' }}>{review.name}</Text>
+        <Text>{review.location.address1}</Text>
+        <Text>{review.location.city}, {review.location.state} {review.location.zip_code}</Text>
+        <Text>{review.phone}</Text>
+        <Text>{review.rating} stars ({review.review_count} reviews)</Text>
+      </View>
+    )}
+  </View>
+  )
+}
+
+export default Short;
+
